cda4f445e89a4bd11a1ea49ffb75650df0bf6ca3
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-01-23 11:23:00 -0500

    Inital application file created.

---
+++ src/java_prg_06_10_a_game_of_twenty/one/JAVA_PRG_06_10_A_Game_of_TwentyOne.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_06_10_a_game_of_twenty.one;
+
+import java.util.Random;
+/**
+ *
+ * @author bluebackdev
+ */
+public class JAVA_PRG_06_10_A_Game_of_TwentyOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
